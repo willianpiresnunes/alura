2c8e30f2c663c64d48fb746dbac0446aec45f088
Willian Pires Nunes <williampiresnunes@hotmail.com>
2019-05-15 22:56:59 -0300

    alterações e notepad ++

--- bytebank-herdado-conta/src/br/com/bytebank/banco/testes/Teste.java
+++ bytebank-herdado-conta/src/br/com/bytebank/banco/testes/Teste.java
@@ -2,18 +2,9 @@ package br.com.bytebank.banco.testes;
 
 public class Teste {
 
-	//Array[]
 	public static void main(String[] args) {
+		// TODO Auto-generated method stub
 
-		int[] idades = new int[5];
-		
-		for (int i = 0; i < idades.length; i++) {
-			idades[i] = i*i;
-		}
-		for (int i = 0; i < idades.length; i++) {
-			System.out.println(idades[i]);
-		}
-		
 	}
 
 }
