07c6f395276869ca947b709a86abc10761387cb6
Willian Pires Nunes <williampiresnunes@hotmail.com>
2019-05-15 23:02:51 -0300

    alterações array

--- bytebank-herdado-conta/src/br/com/bytebank/banco/testes/Teste.java
+++ bytebank-herdado-conta/src/br/com/bytebank/banco/testes/Teste.java
@@ -3,8 +3,15 @@ package br.com.bytebank.banco.testes;
 public class Teste {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		int idades[] = new int [5];
+		
+		int idade1 = 19;
+		int idade2 = 29;
+		int idade3 = 39;
+		int idade4 = 49;
+		int idade5 = 59;
+		
 	}
 
 }
