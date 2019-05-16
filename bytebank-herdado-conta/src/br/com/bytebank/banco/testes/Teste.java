b2ec9a8fe30a8e3555c2754634ba48a36a78761f
Willian Pires Nunes <williampiresnunes@hotmail.com>
2019-05-15 23:12:38 -0300

    alterações array

--- bytebank-herdado-conta/src/br/com/bytebank/banco/testes/Teste.java
+++ bytebank-herdado-conta/src/br/com/bytebank/banco/testes/Teste.java
@@ -4,14 +4,14 @@ public class Teste {
 
 	public static void main(String[] args) {
 
-		int idades[] = new int [5];
-		
-		int idade1 = 19;
-		int idade2 = 29;
-		int idade3 = 39;
-		int idade4 = 49;
-		int idade5 = 59;
-		
+		int[] idades = new int[5];
+
+		for (int i = 0; i < idades.length; i++) {
+			idades[i] = i * i;
+			System.out.println(idades[i]);
+
+		}
+
 	}
 
 }
